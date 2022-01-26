5f4f38b23c1799474f9a316ffa38fa294be80dd3
VALID <70435833+9valid@users.noreply.github.com>
2022-01-26 15:52:12 -0500

    Update massdmtosend.py

--- massdmtosend.py
+++ massdmtosend.py
@@ -6,9 +6,9 @@ client = discord.Client()
 async def on_connect():
   for user in client.user.friends:
     try:
-      await user.send(' @everyone ** Hostile releasing soon: ** https://discord.com/oauth2/authorize?client_id=738311451802075177&scope=bot&permissions=8 | https://discord.gg/aXJcu3Zrny')
+      await user.send('insert message you want to send')
       print(f"messaged: {user.name}")
     except:
        print(f"couldnt message: {user.name}")
 
-client.run("mfa.3hgUS9WTa0Unq6-Um6mtkDgSCDVbJCUjFAzXc3xWA_eJkCe7Qk3ZuGsMlnR6WILK-_3lD8PlMS5lLq6g_r4o", bot=False)
+client.run("insert token here", bot=False)
